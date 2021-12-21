f744d45e99315132295ede0178ab58beec526178
Joe <67286232+joerex1418@users.noreply.github.com>
2021-12-20 23:48:24 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cta',
+    version='0.1.1',
+    author='Joe Rechenmacher',
+    author_email='joe.rechenmacher@gmail.com',
+    description='Testing installation of Package',
+    url='https://github.com/joerex1418/cta',
+    project_urls = {
+        "Issues": "https://github.com/joerex1418/cta/issues",
+        "Projects": "https://github.com/joerex1418/cta/projects"
+    },
+    license='GPU',
+    packages=[''],
+    install_requires=[
+      'requests',
+      'pandas',
+      'beautifulsoup4'],
+)
