e89083568267f9f6c6571aa0909e021f0fe760f6
Joe <67286232+joerex1418@users.noreply.github.com>
2021-12-21 00:15:14 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pycta',
+    name='cta',
     version='0.1.1',
     author='Joe Rechenmacher',
     author_email='joe.rechenmacher@gmail.com',
